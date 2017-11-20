596ee3b9aeb91a38c9566d72236a7cb8d1fdb05a
Emmanuel Horrent <ehorrent@gmail.com>
2017-11-20 22:51:50 +0100

    Started todo list sample

--- src/TodoList/Program.fs
+++ src/TodoList/Program.fs
@@ -1,7 +1,44 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿namespace TodoList
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+open System
+open Elmish
+open Elmish.WPF
+
+module Models =
+    type Priority =
+        | Low
+        | Medium
+        | High
+
+    type Item = 
+        { Name: string; priority: Priority }
+
+    type Model = { Title: string; Items: Item list }
+
+    type Msg =
+        | Add of Item
+        | Remove of Item
+        | Update of oldItem: Item * newItem: Item
+
+module State = 
+    open Models
+
+    let init() = { Title= ""; Items= list.Empty }
+
+    let update (msg:Msg) (model:Model) =
+        match msg with
+        | Add item -> { model with Items = item :: model.Items }
+        | Remove item -> model
+        | Update (oldItem, newItem) -> model
+
+    let view _ _ = 
+        [ "Add" |> Binding.cmd (fun _ m -> Add { Name = "test"; priority=Priority.Medium })
+          "Remove" |> Binding.cmd (fun _ m -> Remove { Name = "test"; priority=Priority.Medium } ) ]
+
+module App =
+    open State
+
+    [<EntryPoint;STAThread>]
+    let main argv = 
+        Program.mkSimple init update view
+        |> Program.runWindow (TodoList.Views.MainWindow())
